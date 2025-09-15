998f7d0d69c9a6312d4a6d16a62ddcc17c8f0c9a
RosyPetrella <petrellamr@gmail.com>
2025-09-16 00:23:07 +0200

    fix: correct typo in snack comment and format type definitions

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-//snak 1
+//snack 1
 let dato: unknown;
 if(typeof dato === 'string') {
   console.log(dato.toUpperCase());
@@ -9,5 +9,27 @@ if(typeof dato === 'string') {
   console.log(dato ? 'si' : 'no');
 } else {
   console.log('tipo non supportato');
+}
+
+//snack 2
+type Dipendente = {
+  nome: string;
+  cognome: string;
+  annoNascita: number;
+  sesso: 'm' | 'f';
+  anniServizio: number[]
+}
+
+//snack 3
+type Developer = Dipendente & {
+  lvExp: 'junior' | 'mid' | 'senior';
+  linguaggi: string[];
+  certigicazioni: string[],
   
+}
+
+type ProjectManager = Dipendente & {
+  teamSize: number | null,
+  budget?: number;
+  stakeholder: string[]
 }
